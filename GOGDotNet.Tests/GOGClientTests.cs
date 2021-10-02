bb1a7c19a7fc27097381e70f263575006ed2ba52
Kamran Ayub <kamran.ayub@gmail.com>
2021-10-02 14:41:03 -0500

    Add failing test coverage for new fields

--- GOGDotNet.Tests/GOGClientTests.cs
+++ GOGDotNet.Tests/GOGClientTests.cs
@@ -1,25 +1,42 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace GOGDotNet.Tests
 {
-    [TestClass]
-    public class GOGClientTests
+  [TestClass]
+  public class GOGClientTests
+  {
+    private GOGClient client;
+
+    [TestInitialize]
+    public void TestInitialize()
     {
-        private GOGClient client;
-
-        [TestInitialize]
-        public void TestInitialize()
-        {
-            this.client = new GOGClient();
-        }
-
-        [TestMethod]
-        public async Task GetGameStats()
-        {
-            const string userID = "MChartier";
-            var response = await this.client.GetGamesStatsAsync(userID);
-            Assert.IsNotNull(response);
-        }
+      this.client = new GOGClient();
     }
+
+    [TestMethod]
+    public async Task GetGameStats()
+    {
+      const string userID = "MChartier";
+      var response = await this.client.GetGamesStatsAsync(userID);
+      Assert.IsNotNull(response);
+      Assert.AreEqual(response.Item1, Models.ProfileState.Verified);
+
+      var games = response.Item2.ToList();
+      var discoElysium = games.FirstOrDefault(g => g.Id == 1771589310);
+
+      Assert.IsNotNull(discoElysium);
+      Assert.IsTrue(discoElysium.AchievementSupport);
+      Assert.IsTrue(discoElysium.Playtime > 0);
+      Assert.IsTrue(discoElysium.AchievementPercentage > 0);
+      Assert.IsTrue(discoElysium.LastSession > DateTime.MinValue);
+
+      var sevenBillionHumans = games.FirstOrDefault(g => g.Id == 2056114425);
+      Assert.IsNotNull(sevenBillionHumans);
+      Assert.IsFalse(sevenBillionHumans.AchievementSupport);
+      Assert.IsNull(sevenBillionHumans.AchievementPercentage);
+    }
+  }
 }
